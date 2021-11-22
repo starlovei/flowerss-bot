a781c49fa84ab4ad777119d1d241678bd1ab13de
starlovei <star_love_i@outlook.com>
2021-11-22 12:43:42 +0800

    publish

--- internal/tgraph/publish.go
+++ internal/tgraph/publish.go
@@ -20,7 +20,7 @@ func PublishHtml(sourceTitle string, title string, rawLink string, htmlContent s
 	//)
 
 	htmlContent = html.UnescapeString(htmlContent) + fmt.Sprintf(
-		"<hr><p>本文章由 <a href=\"https://github.com/indes/flowerss-bot\">flowerss</a> 抓取自RSS，版权归<a href=\"\">源站点</a>所有。</p><p>查看原文：<a href=\"%s\">%s - %s</p>",
+		"<hr><p>查看原文：<a href=\"%s\">%s - %s</p>",
 		rawLink,
 		title,
 		sourceTitle,
